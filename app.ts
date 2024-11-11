2a0bb786b712e7da154cd2c0631dc655cc6909cf
emilioberino <juancaserg@gmail.com>
2024-11-12 00:22:29 +0100

    Basico inicial funcionando con hello world

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import indexRouter from './routes/index.routes';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use("/", indexRouter);
 app.listen(port, () => console.log("La aplicación está corriendo en el puerto 3000"));
